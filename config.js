354a74f9a5f13db446c373d4307ac2a5320c17b5
MasterThien <94881701+MasterThien@users.noreply.github.com>
2022-12-04 22:58:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Happy Birthday';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'click vào ddi';
 const titleCard = '05/12/2003';
-const contentCard = '“Count your life by smiles, not tears. Count your age by friends, not years. Happy birthday!” ...🍰🍻🎉🎊 ';
+const contentCard = '“Count your life by smiles, not tears. Count your age by friends, not years. Happy birthday Hoai Phuong” ...🍰🍻🎉🎊 ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
